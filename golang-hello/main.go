d44b00e8ea968d7986440d57ce292798bbea9972
Tomasz Jonak <tcjonak@gmail.com>
2018-12-17 14:19:39 +0100

    Remove verbose header addition

--- golang-hello/main.go
+++ golang-hello/main.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(respWriter http.ResponseWriter, request *http.Request) {
-		respWriter.WriteHeader(http.StatusOK)
 		respWriter.Write([]byte("Hello, world!"))
 	})
 	http.ListenAndServe("0.0.0.0:80", nil)
